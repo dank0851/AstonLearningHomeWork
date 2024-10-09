ae16919bde5bbe2ee64d1d6aba1075f6d3a94784
DanK0 <dan_k0@mail.ru>
2024-10-09 22:19:36 +0300

    Домашняя работа к лекции №6

--- HomeWork6/src/main/java/com/dank0851/Person.java
+++ HomeWork6/src/main/java/com/dank0851/Person.java
@@ -2,9 +2,7 @@ package com.dank0851;
 
 public class Person {
     public static void main(String[] args) {
-
     }
-
     private String fullName;
     private String position;
     private String eMail;
@@ -34,7 +32,11 @@ public class Person {
 //                + telephoneNumber + " | Salary: " + salary + " | Age: " + age);
 //    }
     public void info1() {
-        System.out.println("Name: " + fullName + "\n | Position: " + position + "\n | email: " + eMail + "\n | Phone: "
-                + telephoneNumber + "\n | Salary: " + salary + "\n | Age: " + age);
+        System.out.println("Name: " + fullName +
+                "\n | Position: " + position +
+                "\n | email: " + eMail +
+                "\n | Phone: " + telephoneNumber +
+                "\n | Salary: " + salary +
+                "\n | Age: " + age);
     }
 }
